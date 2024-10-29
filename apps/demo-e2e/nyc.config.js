7206cdf3c7402136987420fcec94aca91bb78db7
Maxim Belov <belov1988@gmail.com>
2024-10-30 01:06:30 +0200

    wip coverage report

--- apps/demo-e2e/nyc.config.js
+++ apps/demo-e2e/nyc.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  'report-dir': '../../coverage-e2e/libs/ngx-ionic-image-viewer/coverage',
-  'temp-dir': '../../coverage-e2e/libs/ngx-ionic-image-viewer/.nyc_output',
+  extends: '@istanbuljs/nyc-config-typescript',
+  'cache-dir': require('path').join(__dirname, 'node_modules', '.cache', 'nyc'),
+  'report-dir': require('path').join(__dirname, 'coverage'),
+  'temp-dir': require('path').join(__dirname, '.nyc_output'),
   reporter: ['lcov', 'clover', 'json', 'json-summary'],
   extension: ['.js', '.cjs', '.mjs', '.ts', '.tsx', '.jsx'],
   excludeAfterRemap: false
